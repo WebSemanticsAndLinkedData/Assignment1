34dd099a0fe1d8cf8a3f14b2fd4615eaf6d58d30
Luis Fernando Gil Arroyo <luis.gil.arroyo@alumnos.upm.es>
2018-06-03 12:32:52 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Assignment1
+# Assignment 1
+
+Introduction assignment. The file to be uploaded is a csv file with two fields, your github username, and your registration number (número de matricula) in the university. The file has to be called yourGithubUsername.csv
+
+This file will be used to identify your uploads so its **VERY** important to make sure the contents are correct. In this file reports will also be written recording the assignments you delivered and information about them.
+
+### Tests:
+* The csv file exists and it is named correctly
+* The csv file has two fields
---
+++ build/testAssignment1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Name: testCSV.sh
+#Description: Validates student csv file
+#Parameters: none
+#Output: Descriptions of lines with incorrect number of fields, or none if the files are correct
+#Exit Value: Number of errors found, 0 if the file was correct
+
+errors=0
+sleep 2
+username=$(curl -s -H "Authorization: token ${TOKEN}" -X GET "https://api.github.com/repos/${SEMAPHORE_REPO_SLUG}/pulls/${PULL_REQUEST_NUMBER}" | jq -r '.user.login')
+
+if [ ! -f "$username.csv" ]; then
+  echo "File missing. Make sure it has the correct format" "$username.csv" >&2
+  errors=$((errors+1))
+else
+	file=$username.csv
+	numberfields=2
+	awk -v n=$numberfields 'BEGIN{FS=OFS=","} NF==n{count++} NF!=n{print "ERROR in file " FILENAME " line "   count+1 " Incorrect number of fields"; errors++; count++} END {if ( errors != 0  ) {exit 1}}' $file >&2
+	if [[ $? -ne 0 ]]
+	then
+		errors=$((errors+1))
+	fi
+fi
+
+exit $errors
